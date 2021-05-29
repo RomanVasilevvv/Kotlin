6498226c48981d22766cb226d5faff7fb4ce6fcf
Unknown User <rvas2012@yandex.ru>
2021-05-29 23:37:13 +0300

    2 task

--- 14/src/main.kt
+++ 14/src/main.kt
@@ -1,16 +1,26 @@
 import java.lang.Math.abs
-
-fun sumOfNumber(number: Int) : Int
+//задание 1
+/*fun sumOfNumber(number: Int) : Int
         = if (number == 0)
     0
 else
 {
     val digit = number % 10
     sumOfNumber(number / 10) + digit
+}*/
+
+//задание 2 хвостовая рекурсия
+fun sumOfNumber(number: Int, sum: Int = 0) : Int
+        = if (number == 0)
+    sum
+else
+    sumOfNumber(number / 10, sum + number % 10)
+
+fun main(args: Array<String>)
+{
+/*
+
 }
-
-
-
 
 
 fun main(args: Array<String>)
@@ -31,5 +41,25 @@ fun main(args: Array<String>)
     {
         println("Некорректный ввод")
     }*/
+//2 задание
+Хвостовая рекурсия - это особый случай рекурсии, когда вызывающая функция
+больше не выполняет вычисления после выполнения рекурсивного вызова.
+Пример:
+return 5
+return sumOfNumber(a, b)
+return sumOfNumber(a, b + number % 10)
+Антипример:
+return 5 * n
+return n * sumOfNumber(a, b)
+*/
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        println("Сумма цифр числа $number равна ${sumOfNumber(abs(number))}")
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
 
 }
