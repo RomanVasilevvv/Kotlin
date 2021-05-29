8eac6e7a2f8541fcb2d6fb55c335533933531866
Unknown User <rvas2012@yandex.ru>
2021-05-29 23:17:00 +0300

    12 task

--- 13/src/main.kt
+++ 13/src/main.kt
@@ -90,7 +90,7 @@ fun main(args: Array<String>)
 //задание 8.1
 
 // НОД двух чисел
-/*fun nodTwoNumbers(number1: Int, number2: Int, nod: Int = (if (number1 > number2) number2 else number1)) : Int =
+fun nodTwoNumbers(number1: Int, number2: Int, nod: Int = (if (number1 > number2) number2 else number1)) : Int =
     when
     {
         (number1 == 0 || number2 == 0) -> -1
@@ -104,7 +104,8 @@ fun kolvoprost(number: Int, current: Int = 1, count: Int = 0) :Int =
      nodTwoNumbers(number,current)==1 -> kolvoprost(number,current+1,count+1)
      else -> kolvoprost(number, current+1, count)
     }
-fun main()
+
+/*fun main()
 {
    println( kolvoprost(10))
 
@@ -120,7 +121,8 @@ else
     sumOfNumber(number / 10, if (digit%3==0) sum + digit else sum)
 }
 
-fun main(args: Array<String>) {
+    /*fun main(args:Array<String>)
+{
 
     print("Number -> ")
     try {
@@ -129,5 +131,109 @@ fun main(args: Array<String>) {
     } catch (ex: NumberFormatException) {
         println("Некорректный ввод")
     }
+}*/
+//задание 8.3
+fun del(s:Int):Int {
+    var d = 2
+    var max = 0
+    var maxd = 1
+    while (d < s) {
+        if (s % d == 0) {
+            var k = 0
+            var num = s
+            while (num > 0) {
+                var a = 2
+                var flag = false
+                while (a <= num % 10) {
+                    if ((num % 10) % a == 0 && d % a == 0) {
+                        flag = true
+                        break
+                    }
+                    a++
+                }
+                if (!flag) k++
+                num /= 10
+            }
+            if (max < k) {
+                max = k
+                maxd = d
+            }
+        }
+        d++
+    }
+    return maxd
 }
 
+/*fun main()
+{
+    println(del(26))
+}*/
+fun continueMode() : String
+{
+    print("Continue? y/n -> ")
+    return when (readLine()) {
+        "y" -> "mainRelease"
+        "n" -> "return"
+        else -> continueMode()
+    }
+}
+
+fun mainRelease()
+{
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        print("Method 1/2/3 -> ")
+        when(readLine()?.toInt() ?: 1)
+        {
+            1 -> println("Найти количество чисел, взаимно простых с заданным.: ${kolvoprost(number)}")
+            2 -> println("Найти сумму цифр числа, делящихся на 3: ${sumOfNumber(number)}")
+            3 -> println("Найти делитель числа, являющийся взаимно простым с\n" +
+                    "наибольшим количеством цифр данного числа.: ${del(number)}")
+            else -> println("Такого метода нет в списке")
+        }
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
+
+    when(continueMode())
+    {
+        "mainRelease" -> mainRelease()
+        "return" -> return
+    }
+}
+
+/*fun main(args: Array<String>)
+{
+    mainRelease()
+}*/
+
+fun countDel(s:Int):Int {
+    var d = 2
+    var k = 0
+    while (d <= Math.sqrt(s.toDouble())) {
+        if (s % d == 0) k++
+        d++
+    }
+    k *= 2
+    val sqrt: Int = (Math.sqrt(s.toDouble())).toInt()
+    if (Math.pow(sqrt.toDouble(), 2.0) == s.toDouble()) k--
+    return k
+}
+fun treangle():Int {
+    var num = 1
+    var index = 1
+    var kolvo = countDel(num)
+    while (kolvo <= 500) {
+        index++
+        num += index
+        kolvo = countDel(num)
+    }
+    return num
+}
+fun main(args: Array<String>)
+{
+    println(treangle())
+}
